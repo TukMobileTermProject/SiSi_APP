c409d197d0948a475daf9387954c076bd9004bc0
khseon7 <gkrtjs5544@gmail.com>
2023-05-27 14:19:06 +0900

    pwd액티비티 생성 완료 email액티비티 생성

--- app/src/main/java/com/example/sisi/FindemailActivity.kt
+++ app/src/main/java/com/example/sisi/FindemailActivity.kt
@@ -3,9 +3,9 @@ package com.example.sisi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FindemailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_findemail)
     }
 }
