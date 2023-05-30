8a8daa8e4572e5ba26763a8bea37da9cc7265087
khseon7 <gkrtjs5544@gmail.com>
2023-05-30 13:41:25 +0900

    findpassword xml

---
+++ app/src/main/java/com/example/sisi/FindpwdActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sisi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindpwdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_findpwd)
+    }
+}
