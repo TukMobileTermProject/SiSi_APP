5fd2deaa08d1e87998beede98c0ca5d489aceddd
dowoo kim <dowoo12345@naver.com>
2023-06-16 01:22:58 +0900

    FIX: join Failed dialog

--- app/src/main/java/com/example/sisi/JoinActivity.kt
+++ app/src/main/java/com/example/sisi/JoinActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 import java.util.regex.Pattern
 
 class JoinActivity : AppCompatActivity() {
@@ -49,13 +50,14 @@ class JoinActivity : AppCompatActivity() {
                         if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
                             val user = auth.currentUser
-                            val layoutResId = R.layout.dialog_joinsuc
+                            val layoutResId = R.layout.dialog_ok
                             val dialog = AlertDialog.Builder(this)
                                 .setCancelable(false)
                                 .setView(layoutResId)
                                 .create()
                             dialog.show()
-                            dialog.findViewById<Button>(R.id.JoinSucDialogText)?.setOnClickListener {
+                            dialog.findViewById<TextView>(R.id.okDialogTextView)?.setText("회원가입 완료 되었습니다. \\n 로그인 해주세요!!")
+                            dialog.findViewById<Button>(androidx.core.R.id.dialog_button)?.setOnClickListener {
                                 dialog.dismiss()
                                 var outIntent = Intent(this, LoginActivity::class.java)
                                 setResult(Activity.RESULT_OK)
@@ -74,7 +76,7 @@ class JoinActivity : AppCompatActivity() {
                                 .setView(layoutResId)
                                 .create()
                             dialog.show()
-                            dialog.findViewById<TextView>(R.id.okDialogTextView)?.setText("회원가입 완료 되었습니다. \\n 로그인 해주세요!!")
+                            dialog.findViewById<TextView>(R.id.okDialogTextView)?.setText("회원가입에 실패 하였습니다. \\n 이메일 비밀번호를 확인해 주세요.")
                             dialog.findViewById<Button>(R.id.okDialogBtn)?.setOnClickListener {
                                 dialog.dismiss()
                                 var outIntent = Intent(this, LoginActivity::class.java)
